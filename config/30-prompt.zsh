73b7d0ff1fee825d3f859174719009485d28c86b
warrenseymour <wms@fountainhead-solutions.net>
2011-11-15 20:32:39 +0000

    Add git info and newline to prompt

--- config/30-prompt.zsh
+++ config/30-prompt.zsh
@@ -1,8 +1,10 @@
 # Prompts and other decorations
 PROMPT="%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} \
-in %{$fg_bold[green]%}%~%{$reset_color%} "
+in %{$fg_bold[green]%}%~%{$reset_color%} $(git_prompt_info)
+$ "
 
-#ZSH_THEME_GIT_PROMPT_PREFIX="git:(%{$fg[red]%}"
-#ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-#ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗%{$reset_color%}"
-#ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}!"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
