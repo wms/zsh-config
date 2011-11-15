2602be80a0ddb7cd10dc37dec9a7d4393c45cdfe
warrenseymour <wms@fountainhead-solutions.net>
2011-11-15 20:51:29 +0000

    Wrap prompt in precmd function to allow updates

--- config/30-prompt.zsh
+++ config/30-prompt.zsh
@@ -1,10 +1,12 @@
 # Prompts and other decorations
-PROMPT="%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} \
+function precmd {
+    PROMPT="%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} \
 in %{$fg_bold[green]%}%~%{$reset_color%}$(git_prompt_info)
 $ "
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}!"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
+    ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
+    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}!"
+    ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
+    ZSH_THEME_GIT_PROMPT_CLEAN=""
+}
