52abea7b91b0b8836391ad42a89f3d31ee01eee8
Warren Seymour <wms@fountainhead-solutions.net>
2012-10-26 14:51:55 +0100

    Fix incorrect path in bootstrap

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -1,3 +1,3 @@
 # Entry point - load libraries then configs
-for lib_file ($HOME/Dropbox/zsh/lib/*.zsh) source $lib_file
-for config_file ($HOME/Dropbox/zsh/config/*.zsh) source $config_file
+for lib_file ($ZSH_CONF_ROOT/lib/*.zsh) source $lib_file
+for config_file ($ZSH_CONF_ROOT/config/*.zsh) source $config_file
