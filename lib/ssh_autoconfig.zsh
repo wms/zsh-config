2c84dd06c449038dc38f158ca3234405bbc95cf6
Warren Seymour <wms@fountainhead-solutions.net>
2012-12-18 12:31:59 +0000

    Add SSH host autocomplete

---
+++ lib/ssh_autoconfig.zsh
@@ -0,0 +1,8 @@
+h=()
+if [[ -r ~/.ssh/config ]]; then
+  h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+fi
+if [[ $#h -gt 0 ]]; then
+  zstyle ':completion:*:ssh:*' hosts $h
+  zstyle ':completion:*:slogin:*' hosts $h
+fi
